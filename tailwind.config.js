1a559fbdf14a487493ded984e935ef3253b0bb95
wkava57 <wkava57@hotmail.com>
2023-03-22 11:19:49 +0100

    testcarrousel

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+
+  theme: {
+    extend: {},
+  },
+  
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+}
