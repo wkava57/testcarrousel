e85022ed9a68d7bedda18b89fc6e60e1b24e8d9f
wkava57 <wkava57@hotmail.com>
2023-03-23 22:25:40 +0100

    test display text

--- src/pages/maison.tsx
+++ src/pages/maison.tsx
@@ -5,14 +5,14 @@ import { Inter } from 'next/font/google'
 import Image from 'next/image'
 
 
-import avant from '/public/avant.png'
-import apres from '../public/images/apres.png'
-// import image1 from '../public/images/renova.png'
-// import image2 from '../public/images/renovb.png'
-// import image3 from '../public/images/renovc.png'
-// import image4 from '../public/images/renovd.png'
-// import image5 from '../public/images/renove.png'
-// import image6 from '../public/images/renovf.png'
+import avant from '/public/images/avant.png'
+import apres from '/public/images/apres.png'
+import image1 from '/public/images/renova.png'
+import image2 from '/public/images/renovb.png'
+import image3 from '/public/images/renovc.png'
+import image4 from '/public/images/renovd.png'
+import image5 from '/public/images/renove.png'
+import image6 from '/public/images/renovf.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,43 +27,92 @@ export default function Home() {
       </Head>
       <main>
         <div className="title py-12 text-center bg-blue-200">
-          <h1 className='w-[100%] text-5xl font-bold uppercase'>Travaux de renovation</h1>
+          <h1 className=' w-[100%] text-5xl font-bold uppercase'>Travaux de renovation</h1>
 
         </div>
-        <div className="avant">
-          <div className="before w-[70%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
-            xl:w-1/5">
-            <Image className='rounded-2xl ' 
-            src={ avant }alt="une pièce avant la renovation"></Image>
+
+        <div className="home w-full h-screen flex justify-center items-center ">
+          <div className="avant  w-[100%] ">
+            <div className="before w-[75%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
+              xl:w-1/5">
+              <Image className='rounded-2xl ' 
+              src={avant}
+              alt="une pièce avant la renovation"></Image>
+            </div>
+            <br />
+            <h3 className='w-2/4 text-center text-center bg-blue-500 text-white m-auto
+              xl:w-1/5'>
+              Avant
+            </h3>                
+            <br />
           </div>
-          <br />
-          <h3 className='w-3/4 text-center text-center bg-blue-500 text-white m-auto
-            xl:w-1/5'>
-            Avant
-          </h3>                
-          <br />
-        </div>
-        
 
+          <div className="twobloc">
+            <div className=" overflow-hidden m-auto w-[100%*6] w-[45%]  h-[40%]  rounded-2xl shadow-[10px_10px_101px_0_rgb(0,0,0)]">
 
+              <div className="slides  flex shadow-[10px_10px_101px_0_rgb(0,0,0)] animate-animate">
 
-        
-        <div className="avant">
-          <div className="before w-[70%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
-            xl:w-1/5">
-            <Image className='rounded-2xl ' 
-            src={ apres } alt="une pièce avant la renovation"></Image>
+                <Image className='w-[100%] slide float-left text-center'
+                  src={image1} alt="première diapo de la carrousel"></Image>
+                  
+                <Image className='w-[100%] slide float-left'
+                  src={image2} alt="deuxième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image3} alt="troisième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image4} alt="quatrième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image5} alt="cinquième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image6} alt="sixième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image1} alt="retour à la première diapo"></Image>
+
+              </div>
+            </div>
+            <br /><br />
+            
+              
+                <div className="carrousel text-center font-[30px] leading-[45px] ">
+                  <div className="pretxt h-[5px] w-1/2 text-blue-500 font-bold shadow-black uppercase ">je vous présente :</div>
+                  <div className="changetxt text-center h-[45px] overflow-hidden ">
+                    <div className="content font-bold font- uppercase animate-carrousel">
+                      <div className="element">Peinture chambre 1</div>
+                      <div className="element">Préparation de bande de protection</div>
+                      <div className="element">Pose de placo salon</div>
+                      <div className="element">Pièce de stockage de matériels</div>
+                      <div className="element">Ponçage du plafond</div>
+                      <div className="element">Chambre 2 en travaux</div>
+                      <div className="element">Peinture chambre 1</div>
+                    </div>
+                  </div>
+                </div>
+              
+           
+            
+            
+            
           </div>
-          <br />
-          <h3 className='w-3/4 text-center text-center  bg-blue-400 text-white m-auto
-            xl:w-1/5'>
-            Avant
-          </h3>                
-          <br />
-        </div>
-        <div>
+          
 
-        </div>
+
+
+
+          <div className="apres w-[100%] ">
+            <div className="after w-[75%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
+              xl:w-1/5">
+              <Image className='rounded-2xl ' 
+              src={apres} alt="une pièce avant la renovation"></Image>
+            </div>
+            <br />
+            <h3 className='w-2/4 text-center text-center  bg-blue-400 text-white m-auto
+              xl:w-1/5'>
+              Apres
+            </h3>                
+            <br />
+          </div>
+
+        </div>         
         
 
       </main>  
